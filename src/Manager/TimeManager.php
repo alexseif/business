6cb78eb80e47ff0b046ce514c5ee628154b28890
Alex Seif <alex.seif@gmail.com>
2023-01-12 15:43:29 +0200

    removing dumps

--- src/Manager/TimeManager.php
+++ src/Manager/TimeManager.php
@@ -83,14 +83,12 @@ class TimeManager
     public function addToNow()
     {
         $newEta = clone $this->getNow()->modify('+' . $this->getPrevDuration() . ' minutes');
-        dump($newEta);
         //@todo: might be more than a day
         //@todo: check workdays
         if ($newEta->format('H') > $this->sunset) {
             $this->getNow()->modify('+1 day')
                 ->setTime($this->getSunrise(), 00);
             $newEta = clone $this->getNow()->modify('+' . $this->getPrevDuration() . ' minutes');
-            dump($newEta);
         }
         return $newEta;
     }
@@ -98,7 +96,6 @@ class TimeManager
     public function setETA(TimeManagement $item)
     {
         if (is_null($item->getCompleted())) {
-            dump($item->getTask());
             $item->setEta($this->addToNow());
             $this->setPrevDuration($item->getDuration() ?: 60);
         }
